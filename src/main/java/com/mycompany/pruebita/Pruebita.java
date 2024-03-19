ec90d06bb9303ed1fa5fa69b27a0307f208768be
develasmen <develasmen@localhost>
2024-03-19 10:35:27 -0600

    Pruebita

---
+++ src/main/java/com/mycompany/pruebita/Pruebita.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pruebita;
+
+/**
+ *
+ * @author develasmen
+ */
+public class Pruebita {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
